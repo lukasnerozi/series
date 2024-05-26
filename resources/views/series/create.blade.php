c2f4c07a48571f1b554ca6a32f81104e5526b2b3
Lukas Nerozi <luka.100.nerozi@gmail.com>
2024-05-26 20:38:42 -0300

    [ADD] Adição de uma página de criação de séries

---
+++ resources/views/series/create.blade.php
@@ -0,0 +1,7 @@
+<x-layout title="Criar séries">
+  <form method="POST">
+    <label for="nome">nome:</label>
+    <input type="text" name="nome" id="nome" placeholder="Nome da série"/>
+
+  </form>
+</x-layout>
