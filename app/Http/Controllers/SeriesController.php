2a2252fbe9564c1752546a8784cf00ad1e3986f7
Lukas Nerozi <luka.100.nerozi@gmail.com>
2024-05-26 20:40:00 -0300

    [MOD] Ajuste na formatação (Identação de código)

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -6,14 +6,19 @@ use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    public function index(Request $request){
+  public function index(Request $request)
+  {
 
-        $series = [
-            'Punisher',
-            'Lost',
-            'Greys\'s anatomy'
-        ];
-        
-       return view('series.index')->with('series', $series);
-    }
+    $series = [
+      'Punisher',
+      'Lost',
+      'Greys\'s anatomy'
+    ];
+    return view('series.index')->with('series', $series);
+  }
+
+  public function create(Request $request)
+  {
+    return view('series.create');
+  }
 }
